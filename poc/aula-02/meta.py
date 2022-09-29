3b6d34a80398d58cbc8600d5e1407ecad354ff16
Pedro Moura <pedrorpmoura@gmail.com>
2022-09-30 00:59:40 +0100

    POC

---
+++ poc/aula-02/meta.py
@@ -0,0 +1,31 @@
+
+titulo = input("Insira o título: ")
+autor = input("Insira o autor: ")
+ano = input("Insira o ano: ")
+formato = input("Insira o formato: ")
+nome_ficheiro = input("Insira o nome do ficheiro: ")
+
+if formato == "YAML":
+
+	extensao = ".yml"
+
+	f = open(nome_ficheiro + extensao, "w")
+
+	f.write("titulo: " + titulo)
+	f.write("autor: " + autor)
+	f.write("ano: " + ano)
+
+	f.close()
+
+else:
+	extensao = ".json"
+
+	f = open(nome_ficheiro + extensao, "w")
+
+	f.write("{\n")
+	f.write("\t\"titulo\": \"" + titulo + "\",\n")
+	f.write("\t\"autor\": \"" + autor + "\",\n")
+	f.write("\t\"ano\": \"" + ano + "\"\n")
+	f.write("}\n")
+
+	f.close()
